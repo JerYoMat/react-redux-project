79593fbbaadff4229f5c49ce8da011c33b123466
JerYOMat <jeremy.emata@gmail.com>
2019-03-27 15:27:06 -0400

    successfully implemented looking up the specific company

--- client/src/pages/CompanyAnalysisPage.js
+++ client/src/pages/CompanyAnalysisPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const CompanyAnalysisPage = ({ companyId }) => {
-  return <div>Viewing {companyId}</div>;
+const CompanyAnalysisPage = ({ companyId, company, loading }) => {
+  if (loading) {
+    return <div>...Loading...</div>
+  }
+  return <div>Viewing {companyId} -- {company.name}</div>;
 };
 
-export default CompanyAnalysisPage;
+const mapState = (state, ownProps) => {
+  return {
+    loading: state.companies.loadingCompanies,
+    company: state.companies.find(c => c.id === parseInt(ownProps.companyId, 10))
+  }
+}
+
+export default connect(mapState)(CompanyAnalysisPage);
