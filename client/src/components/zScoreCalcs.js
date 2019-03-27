0cba0f2f0574497e9e2f22373b35efdf5a39aa3d
JerYOMat <jeremy.emata@gmail.com>
2019-03-27 18:43:22 -0400

    added zScoreCalcs returns JS object with normalized a,b,c,d,e values

--- client/src/components/zScoreCalcs.js
+++ client/src/components/zScoreCalcs.js
@@ -8,21 +8,23 @@ const zScoreCalcs = (company) => {
     e: null
   }
   // Raw Calcs
-  scoreObj[a] = company.ebit / company.total_assets
-  scoreObj[b] = company.revenue / company.total_assets
-  scoreObj[c] = (company.total_assets - company.total_liabilities) / 
-
+  scoreObj.a = 3.107 * (company.ebit / company.total_assets);
+  scoreObj.b = 0.998 * (company.revenue / company.total_assets);
+  scoreObj.c = 0.420 * ((company.total_assets - company.intangible_assets - company.total_liabilities) / company.total_liabilities)
+  scoreObj.d = 0.717 * ((company.current_assets - company.current_liabilities) / company.total_assets)
+  scoreObj.e = 0.847 * (company.retained_earnings / company.total_assets)
 
   //Add bounds -4 to -8
-
+  for (let property in scoreObj) {
+    if (scoreObj[property] < -4) {
+      scoreObj[property] = -4
+    }
+    if (scoreObj[property] > 8) {
+      scoreObj[property] = 8
+    }
+  }  
   return scoreObj
 }
-t.integer "fiscal_year"
-t.integer "revenue"
-t.integer "ebit"
-t.integer "current_assets"
-t.integer "current_liabilities"
-t.integer "total_assets"
-t.integer "total_liabilities"
-t.integer "retained_earnings"
-export zScoreCalcs;
+
+
+export default zScoreCalcs;
