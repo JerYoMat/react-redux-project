f2f34a648ddbde32c149040ab9e0fa1788cc43e4
JerYOMat <jeremy.emata@gmail.com>
2019-03-27 04:36:14 -0400

    added immer and added skeleton for reducer

---
+++ client/src/reducer.js
@@ -0,0 +1,13 @@
+import produce from 'immer';
+
+const initialState = {
+  companies: []
+}
+
+const reducer = produce((draft, action) => {
+  switch(action.type) {
+    
+  }
+}, initialState)
+
+export default reducer;
